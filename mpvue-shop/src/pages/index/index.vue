f4978b3b2106b3c1ae5b776ad0cf708d13e3cd01
hhl1206 <1919412022@qq.com>
2020-02-10 21:35:42 +0800

    网易严选7，8首页开发

--- mpvue-shop/src/pages/index/index.vue
+++ mpvue-shop/src/pages/index/index.vue
@@ -8,21 +8,67 @@
         <span class="icon"></span>
       </div>
     </div>
-  </div>
+    <div class="swiper">
+      <swiper class="swiper-container" indicator-dots="true" autoplay="true" interval="3000" circular="true" duration="500">
+        <block v-for="(item, index) in banner" :key="index">
+          <swiper-item class="swiper-item">
+            <image class="slide-image" :src="item.image_url" />
+          </swiper-item>
+        </block>
+        <!-- block标签，无具体含义，通常用来做for循环.
+        <swiper-item>代表每一张轮播图 
+        写了banner数据源中就要记得加一个banner,从哪来？
+        src要用v-bind绑定一下,因为是动态要动态获取的-->
+      </swiper>
+    </div>
+    <div class="channel">
+        <div v-for="(item,index) in channel" :key="index" :@click="categroyList(item.id)"> 
+          <img :src="item.icon_url" alt="" />
+          <p>{{item.name}}</p> 
+          <!-- image样式占的位置太大了，去调样式 
+          加了for中的channel数组就要在数据源中添加,channel数据源也需要从接口中获取来，那就继续去做接口请求-->
+        </div>
+    </div>
+    <div class="brand">
+      <div class="head">
+        品牌制造商直供
+      </div>
+      <div class="content">
+        <div v-for="(item,index) in brandList" :key="index" @click="branddetail(item.id)" >
+          <div>
+            <!-- 每一小块 -->
+            <p>{{item.name}}</p>
+            <p class="price">{{item.floor_price}}元起</p>
+          </div>
+          <img :src="item.new_pic_url" alt="" >
+        </div>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
 import amapFile from '../../utils/amap-wx.js'
 import { mapState,mapMutations } from 'vuex'
+import { get } from '../../utils'  //导入封装的get请求(在index.js里)
+// 后面不加index,因为会默认加上/index.js
 export default {
   data (){
     return{
       // cityName:'南昌'
+      banner:[],
+      channel:[],
+      brandList:[]
     }
   },
   computed:{ //计算属性
   // 在计算属性computed中解构，将mapState解构，解构出数据源里的cityName
     ...mapState(['cityName']) //可以直接拿到页面中展示<div @click="toMappage">{{cityName}}</div>
+  },
+  // 一进去首页，benner图就要出现。所以请求这个数据的方法应该是自执行的。也就意味着要放到生命周期函数中
+  mounted(){
+    this.getData()
+    this.getCityName()
   },
   methods:{
     ...mapMutations(['update']),
@@ -70,6 +116,33 @@ export default {
           // _this.cityName='北京'
           _this.update({cityName:'北京'})
         }
+      })
+    },
+    
+    async getData(){
+      // wx.request 封装接口请求，去utils
+      // 这里是取数据源的，那就应该调用刚写的接口，那么就需要在当前页面引入get,post这两个方法
+      // 导航的banner图我们采用get方法，那么导入get
+      // 此时的get的url可以写成任何样子，因为此处的路径是等会做后端开发时自己定义的路径，记得等会要与后端的一致
+      const data=await get('/index/index') //后端必须有一个http://localhost:5757/lm/index/index
+      // 这里的get方法是封装的请求
+      // 拿数据
+      console.log(data)//现在还看不到拿到的数据是什么，因为还没写后端的项目
+      // 那么开始后端 用koa2提供/index/index接口给我们这里使用
+      // 数据库的数据能输出了之后，接下来打通banner图的前后端协作连接，将数据成功渲染到页面中去。
+      //把拿到的数据给到数据源中的banner[]
+      this.banner = data.banner
+      this.channel = data.channel
+      this.bandList = data.bandList
+    },
+    categroyList(id){ //有了id才知道具体要跳到哪里
+      wx.navigateTo({
+        url:'/pages/categroyList/main?id=' +id
+      })
+    },
+    branddetail(id){
+      wx.navigateTo({
+        url:'/pages/branddetail/main?id=' +id
       })
     }
   }

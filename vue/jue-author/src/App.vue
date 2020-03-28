9a8663fb4484fe9dabdb5b0290c77241f984f7ba
hhl1206 <1919412022@qq.com>
2020-03-29 00:29:21 +0800

    掘金作者榜 未完

--- vue/jue-author/src/App.vue
+++ vue/jue-author/src/App.vue
@@ -5,18 +5,52 @@
       <div class="head">
         作者榜
       </div>
-      <div class="content">
-        <img src="http://b-ssl.duitang.com/uploads/item/201802/20/20180220165946_RiGPS.thumb.700_0.jpeg" alt="">
+      <div class="content" v-for="(item,index) in author" :key="index">
+        <!-- <img src="http://b-ssl.duitang.com/uploads/item/201802/20/20180220165946_RiGPS.thumb.700_0.jpeg" alt="">
         <div class="author">
-          <p>jonne</p>
+          <p style="font-size:20px; font-wight:blod; color:#000;">jonne</p>
           <p>掘金能手</p>
           <p>掘金作家</p>
+        </div> -->
+        <img :src="item.img" alt="">
+        <div class="author">
+          <p style="font-size:20px; font-wight:blod; color:#000;">{{item.name}}</p>
+          <p>{{item.intro}}</p>
+          <p>{{item.iden}}</p>
         </div>
       </div>
+      <div class="button">完整榜单 > </div>
     </div>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      author: [
+        {
+        img: 'http://b-ssl.duitang.com/uploads/item/201802/20/20180220165946_RiGPS.thumb.700_0.jpeg',
+        name: "网",
+        intro: "掘金",
+        iden: "作家"
+        },
+        {
+        img: 'http://b-ssl.duitang.com/uploads/item/201802/20/20180220165946_RiGPS.thumb.700_0.jpeg',
+        name: "网",
+        intro: "掘金",
+        iden: "作家"
+        },
+        {
+        img: 'http://b-ssl.duitang.com/uploads/item/201802/20/20180220165946_RiGPS.thumb.700_0.jpeg',
+        name: "网",
+        intro: "掘金",
+        iden: "作家"
+        }
+      ]
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -34,6 +68,7 @@
   font-size: 36px;
 }
 .nav{
+  width: 20%;
   padding: 5px 20px;
   margin-right: 300px;
   float: right;
@@ -42,20 +77,33 @@
 }
 .head{
   text-align: left;
-  padding: 40px 0;
-  border: 1px solid #fff;
+  padding: 10px 20px;
+  border: 1px solid #F0F0F0;
 }
 .content{
   width: 730rpx;
-  margin: 0 auto;
+  height: 30%;
+  margin: 2px auto;
   display: flex;
-  border: 1px solid #fff;
+  border: 1px solid #F0F0F0;
 }
 img{
   /* display: inline-block; */
-  width: 30px;
-  height: 10%;
+  width: 30%;
+  height: 30%;
+  padding: 6px;
+}
 
+p{
+  margin: 4px auto;
+  font-size: 16px;
+  color: #909090;
+}
+.button{
+  text-align: left;
+  padding: 10px 20px;
+  border: 1px solid #F0F0F0;
+  color: #007FFF;
 }
 
 
